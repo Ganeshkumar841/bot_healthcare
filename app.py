df5aeae32dc1322f12439f73f3baf81315ffb1f4
Ganeshkumar841 <ganeshkumarbasa@gmail.com>
2025-11-13 20:00:28 +0530

    Gemini model changed

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ else:
 EMBEDDING_MODEL = "models/text-embedding-004"
 # --- MODIFIED: Use a valid and current generative model ---
 GENERATIVE_MODEL = "gemini-2.5-flash-lite"
-#need to change to the new model after some time
+
 
 
 # --- FAISS Index (RAG) Loading ---
